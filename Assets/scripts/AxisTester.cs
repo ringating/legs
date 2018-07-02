7af6f17c086a5b0f3a980635374a9e7e4f36f74a
ringating <ringatingaling@gmail.com>
2018-07-02 01:55:02 -0700

    so far

---
+++ Assets/scripts/AxisTester.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisTester : MonoBehaviour
+{
+	public Transform raw;
+	public Transform processed;
+	public InputHandler input;
+
+	private float scalar = 4;
+
+	void Update ()
+	{
+		raw.position = new Vector3(Input.GetAxisRaw("left stick x")*scalar, Input.GetAxisRaw("left stick y")*scalar, 0);
+		processed.position = input.move * scalar;
+	}
+}
---
+++ Assets/scripts/InputHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+	private float deadZone = 0.15f;
+	private float liveZone = 0.975f;
+
+	public Vector2 move;
+	public Vector2 aim;
+
+	void Update()
+	{
+		UpdateInputs();
+	}
+
+	private void ResetInputs()
+	{
+		move = Vector2.zero;
+		aim = Vector2.zero;
+	}
+
+	private void UpdateInputs()
+	{
+		ResetInputs();
+
+		move.Set(Input.GetAxisRaw("left stick x"), Input.GetAxisRaw("left stick y"));
+		move = move.normalized * Map(move.magnitude, deadZone, liveZone, 0, 1);
+	}
+
+	public float Map(float val, float inMin, float inMax, float outMin, float outMax)
+	{
+		return Mathf.Lerp(outMin, outMax, Mathf.InverseLerp(inMin, inMax, val));
+	}
+}
