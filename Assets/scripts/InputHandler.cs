f34e89e5f30aabb5d66ee5279fb4ccd0b7af6374
ringating <34548930+ringating@users.noreply.github.com>
2018-07-03 11:29:48 -0700

    added aim axis update code, added vars for first button

--- Assets/scripts/InputHandler.cs
+++ Assets/scripts/InputHandler.cs
@@ -7,8 +7,14 @@ public class InputHandler : MonoBehaviour
 	private float deadZone = 0.15f;
 	private float liveZone = 0.975f;
 
+	// axes
 	public Vector2 move;
 	public Vector2 aim;
+
+	// buttons
+	public bool hop;
+	public bool hopDown;
+	public bool hopUp;
 
 	void Update()
 	{
@@ -27,6 +33,11 @@ public class InputHandler : MonoBehaviour
 
 		move.Set(Input.GetAxisRaw("left stick x"), Input.GetAxisRaw("left stick y"));
 		move = move.normalized * Map(move.magnitude, deadZone, liveZone, 0, 1);
+
+		aim.Set(Input.GetAxisRaw("right stick x"), Input.GetAxisRaw("right stick y"));
+		aim = aim.normalized * Map(aim.magnitude, deadZone, liveZone, 0, 1);
+
+
 	}
 
 	public float Map(float val, float inMin, float inMax, float outMin, float outMax)
