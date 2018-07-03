5798d392035eb2635f3d922d05676fbd399126d2
ringating <34548930+ringating@users.noreply.github.com>
2018-07-03 12:48:33 -0700

    more

--- Assets/scripts/InputHandler.cs
+++ Assets/scripts/InputHandler.cs
@@ -23,21 +23,31 @@ public class InputHandler : MonoBehaviour
 
 	private void ResetInputs()
 	{
+		// axes
 		move = Vector2.zero;
 		aim = Vector2.zero;
+
+		// buttons
+		hop = false;
+		hopDown = false;
+		hopUp = false;
 	}
 
 	private void UpdateInputs()
 	{
 		ResetInputs();
 
+		// axes
 		move.Set(Input.GetAxisRaw("left stick x"), Input.GetAxisRaw("left stick y"));
 		move = move.normalized * Map(move.magnitude, deadZone, liveZone, 0, 1);
 
 		aim.Set(Input.GetAxisRaw("right stick x"), Input.GetAxisRaw("right stick y"));
 		aim = aim.normalized * Map(aim.magnitude, deadZone, liveZone, 0, 1);
 
-
+		// buttons
+		hop = Input.GetButton("hop");
+		hopDown = Input.GetButtonDown("hop");
+		hopUp = Input.GetButtonUp("hop");
 	}
 
 	public float Map(float val, float inMin, float inMax, float outMin, float outMax)
